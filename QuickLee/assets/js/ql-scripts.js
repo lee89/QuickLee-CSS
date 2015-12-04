2c175881f935bdb29429f97cd2e1e29e0b37330b
Lee Price <you@example.com>
2015-12-04 18:49:53 +0000

    Renamed compiled JavaScript

--- QuickLee/assets/js/ql-scripts.js
+++ QuickLee/assets/js/ql-scripts.js
@@ -1,19 +1,29 @@
 var quickLee = {
 
 	init: function(){
+		// Activate responsive menu
 		this.responsiveMenu();
 	},
 
 	responsiveMenu: function(){
 
+		// Create toggle button
 		$('<a class="toggle-menu" href="#">MENU</a>').prependTo('.main-menu').next('ul').addClass('hidden');
 
+		// Toggle menu
 		$(document).on('click', '.toggle-menu', function(){
 			$('.main-menu ul').toggleClass('hidden');
 		});
 
 	}
 
+	/**
+	 * yourJavascript: function(){
+	 * 	your functions here
+	 * }
+	 */
+
 };
 
+// Initialise QuickLee-CSS javascript
 quickLee.init();
